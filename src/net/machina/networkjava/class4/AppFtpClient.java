5fcb02f598e5263a0194c4f40f7886e55cc149f6
Machina123 <machina123@users.noreply.github.com>
2020-04-29 18:24:33 +0200

    [Class 4] FTP methods added

--- src/net/machina/networkjava/class4/AppFtpClient.java
+++ src/net/machina/networkjava/class4/AppFtpClient.java
@@ -3,6 +3,7 @@ package net.machina.networkjava.class4;
 import com.jcraft.jsch.*;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class AppFtpClient {
 
@@ -23,7 +24,7 @@ public class AppFtpClient {
     }
 
     public void Start() {
-        if (ConnectToFTP()) {
+        if (connect()) {
             boolean working = true;
             System.out.println("Nawiązano połączenie serwerem ");
             System.out.println("Proszę wybrać opcję:");
@@ -43,37 +44,38 @@ public class AppFtpClient {
                         case "1":
                             System.out.println("Podaj lokliację lub pozostaw puste");
                             String location = input.nextLine();
-                            ShowAllFile(location);
+                            listAllFiles(location);
                             break;
                         case "2":
                             System.out.println("Podaj lokalizację pliku na serwerze:");
                             String servweFile = input.nextLine();
                             System.out.println("Podaj loklizację do zapisania ");
                             String locationDest = input.nextLine();
-                            DownloadFile(servweFile, locationDest);
+                            downloadFile(servweFile, locationDest);
                             break;
                         case "3":
                             System.out.println("Podaj lokalizację pliku z dysku:");
                             String localFile = input.nextLine();
                             System.out.println("Podaj lokalizację na serwerze:");
                             String serverDest = input.nextLine();
-                            UpLoadFile(localFile, serverDest);
+                            uploadFile(localFile, serverDest);
                             break;
                         case "4":
                             System.out.println("Proszę o podanie pliku do usunięcia:");
                             String delFile = input.nextLine();
+                            removeFile(delFile);
                             break;
                         case "5":
                             System.out.println("Proszę podać plik do zmiany nazwy:");
                             String oldName = input.nextLine();
                             System.out.println("Proszę podać nową nazwe pliku:");
                             String newName = input.nextLine();
-                            RenameFile(channelSftp.pwd(), oldName, newName);
+                            renameFile(channelSftp.pwd(), oldName, newName);
                             break;
                         case "6":
-                            System.out.println("Proszę podaćkatalog:");
+                            System.out.println("Proszę podać katalog:");
                             String folder = input.nextLine();
-                            MoveFromCatalog(folder);
+                            changeDirectory(folder);
                             break;
                         case "q":
                             System.out.println("kończę prace");
@@ -87,10 +89,137 @@ public class AppFtpClient {
                     e.printStackTrace();
                 }
             }
-            DisconnectFTP();
+            disconnect();
         } else {
             System.out.println("Nie udało się nawiązać połączenia z serwerem: ");
             System.out.println(host);
         }
     }
+
+    public boolean connect() {
+        try {
+            session = jsch.getSession(ftpData.GetLogin(), host, 22);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword(ftpData.GetPassword());
+            session.connect(10000);
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            home = channelSftp.getHome();
+            System.out.println(home);
+            return true;
+        } catch (JSchException | SftpException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void disconnect() {
+        channel.disconnect();
+        session.disconnect();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void listAllFiles(String location) {
+        try {
+            String tempLoc;
+            if(location.isEmpty()) tempLoc = channelSftp.pwd();
+            else tempLoc = channelSftp.pwd() + "/" + location;
+            Vector files = channelSftp.ls(tempLoc);
+            for (Object file : files) {
+                ChannelSftp.LsEntry lsEntry = (ChannelSftp.LsEntry) file;
+                if(!lsEntry.getFilename().endsWith("."))
+                    System.out.println(lsEntry.getFilename() + " " + lsEntry.getAttrs().getSize());
+            }
+        } catch (SftpException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void changeDirectory(String location) {
+        try {
+            channelSftp.cd(location);
+        } catch (SftpException e) {
+            System.out.println("Błąd: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void downloadFile(String remotePath, String localPath) {
+        try {
+            String tempLoc = null;
+            if(remotePath.contains("/")) {
+                if(channelSftp.ls(channelSftp.pwd() + "/" + remotePath).size() > 0) {
+                    tempLoc = channelSftp.pwd() + "/" + remotePath;
+                }
+            } else {
+                tempLoc = channelSftp.pwd() + "/" + remotePath;
+            }
+            channelSftp.get(tempLoc, localPath);
+            System.out.println("Plik pobrany");
+        } catch (SftpException e) {
+            e.printStackTrace();
+            System.out.println("Nie udało się pobrać pliku: " + e.getMessage());
+        }
+    }
+
+    public void uploadFile(String localPath, String remotePath) {
+        try {
+            String[] tempLoc;
+            if(remotePath.contains("/")) {
+                tempLoc = remotePath.split("/");
+                for(String pathPart : tempLoc) {
+                    if(pathPart.length() > 0 || pathPart.contains(".")) {
+                        try {
+                            channelSftp.cd(pathPart);
+                        } catch (SftpException e) {
+                            System.out.println("Katalog " + pathPart + " nie istnieje. Utworzyć? (T/N)");
+                            Scanner scanner = new Scanner(System.in);
+                            if(scanner.nextLine().equalsIgnoreCase("t")) {
+                                channelSftp.mkdir(pathPart);
+                                channelSftp.cd(pathPart);
+                                System.out.println("Katalog utworzony: " + pathPart);
+                            } else {
+                                System.out.println("Wysyłanie przerwane");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            channelSftp.put(localPath, channelSftp.pwd());
+            System.out.println("Plik wysłany");
+        } catch (SftpException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeFile(String filename) {
+        try {
+            String path = channelSftp.pwd() + "/" + filename;
+            if(channelSftp.ls(path).size() > 0){
+                System.out.println("Operacja jest nieodwracalna!");
+                System.out.println("Czy na pewno chcesz usunąć plik " + filename + "? (T/N)");
+                Scanner scanner = new Scanner(System.in);
+                if(scanner.nextLine().equalsIgnoreCase("t")) {
+                    channelSftp.rm(filename);
+                    System.out.println("Plik usunięty");
+                }
+            }
+        } catch (SftpException e) {
+            e.printStackTrace();
+            System.out.println("Nie można usunąć pliku: " + e.getMessage());
+        }
+    }
+
+    public void renameFile(String location, String oldName, String newName) {
+        try {
+            channelSftp.rename(location + "/" + oldName, location + "/" + newName);
+            System.out.println("Zmieniono nazwę");
+        } catch (SftpException e) {
+            e.printStackTrace();
+            System.out.println("Nie udało się zmienić nazwy: " + e.getMessage());
+        }
+    }
+
 }
--- src/net/machina/networkjava/class4/Main.java
+++ src/net/machina/networkjava/class4/Main.java
@@ -2,6 +2,6 @@ package net.machina.networkjava.class4;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: FTP client execution
+        new AppFtpClient("ux.up.krakow.pl");
     }
 }
